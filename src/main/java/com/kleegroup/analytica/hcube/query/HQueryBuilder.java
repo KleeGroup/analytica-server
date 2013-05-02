d188ecbac2eed77d88924b840e09201df85c29d6
Philippe Chrétien <pchretien55@gmail.com>
2013-05-02 14:08:33 +0200

    assert

--- src/main/java/com/kleegroup/analytica/hcube/query/HQueryBuilder.java
+++ src/main/java/com/kleegroup/analytica/hcube/query/HQueryBuilder.java
@@ -3,6 +3,7 @@ package com.kleegroup.analytica.hcube.query;
 import java.util.Date;
 
 import kasper.kernel.lang.Builder;
+import kasper.kernel.util.Assertion;
 
 import com.kleegroup.analytica.hcube.dimension.HCategoryPosition;
 import com.kleegroup.analytica.hcube.dimension.HTimeDimension;
@@ -12,7 +13,6 @@ import com.kleegroup.analytica.hcube.dimension.HTimeDimension;
  * @author npiedeloup, pchretien
  */
 public final class HQueryBuilder implements Builder<HQuery> {
-	//---	
 	private HTimeDimension timeDimension;
 	private Date from;
 	private Date to;
@@ -21,21 +21,33 @@ public final class HQueryBuilder implements Builder<HQuery> {
 	private String[] subCategories;
 
 	public HQueryBuilder on(HTimeDimension timeDimension) {
+		Assertion.notNull(timeDimension);
+		Assertion.isNull(this.timeDimension);
+		//---------------------------------------------------------------------
 		this.timeDimension = timeDimension;
 		return this;
 	}
 
 	public HQueryBuilder from(Date date) {
+		Assertion.notNull(date);
+		Assertion.isNull(from);
+		//---------------------------------------------------------------------
 		from = date;
 		return this;
 	}
 
 	public HQueryBuilder to(Date date) {
+		Assertion.notNull(date);
+		Assertion.isNull(to);
+		//---------------------------------------------------------------------
 		to = date;
 		return this;
 	}
 
 	public HQueryBuilder with(final String type, String... subCategories) {
+		Assertion.notNull(type);
+		Assertion.isNull(this.type);
+		//---------------------------------------------------------------------
 		this.type = type;
 		this.subCategories = subCategories;
 		return this;
