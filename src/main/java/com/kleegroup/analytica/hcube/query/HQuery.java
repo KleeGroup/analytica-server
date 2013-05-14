2079980a7e96fe2f4a91719b975275dfce2dc5b2
Stephane TATCHUM <tksteph12@gmail.com>
2013-05-14 10:29:28 +0200

    query

--- src/main/java/com/kleegroup/analytica/hcube/query/HQuery.java
+++ src/main/java/com/kleegroup/analytica/hcube/query/HQuery.java
@@ -4,8 +4,8 @@ import java.util.List;
 
 import kasper.kernel.util.Assertion;
 
-import com.kleegroup.analytica.hcube.dimension.HTimePosition;
 import com.kleegroup.analytica.hcube.dimension.HCategoryPosition;
+import com.kleegroup.analytica.hcube.dimension.HTimePosition;
 
 /**
  * Requête permettant de définir les zones de sélections sur les différents axes du cube.
@@ -14,27 +14,29 @@ import com.kleegroup.analytica.hcube.dimension.HCategoryPosition;
  */
 public final class HQuery {
 	private final HTimeSelection timeSelection;
-	private final HCategoryPosition categoryPosition;
+	private final HCategorySelection categorySelection;
 
-	HQuery(HTimeSelection timeSelection, HCategoryPosition categoryPosition) {
+	HQuery(HTimeSelection timeSelection, HCategorySelection  categorySelection) {
 		Assertion.notNull(timeSelection);
-		Assertion.notNull(categoryPosition);
+		Assertion.notNull(categorySelection);
 		//---------------------------------------------------------------------
 		this.timeSelection = timeSelection;
-		this.categoryPosition = categoryPosition;
+		this.categorySelection = categorySelection;
 	}
 
 	//-----------------------What----------------------------------------------
-	public HCategoryPosition getCategoryPosition() {
-		return categoryPosition;
+	public List<HCategoryPosition> getAllCategoryPositions(){
+		return categorySelection.getAllCategoryPositions();
 	}
-
+	
+	
 	//-----------------------When----------------------------------------------
 	public List<HTimePosition> getAllTimePositions() {
 		return timeSelection.getAllTimePositions();
 	}
-
+	
+	
 	public String toString() {
-		return ("query : {" + timeSelection + " with:" + getCategoryPosition() + "}");
+		return ("query : {" + timeSelection + " with:" + categorySelection + "}");
 	}
 }
--- src/main/java/com/kleegroup/analytica/hcube/query/HQueryBuilder.java
+++ src/main/java/com/kleegroup/analytica/hcube/query/HQueryBuilder.java
@@ -5,6 +5,7 @@ import java.util.Date;
 import kasper.kernel.lang.Builder;
 import kasper.kernel.util.Assertion;
 
+import com.kleegroup.analytica.hcube.HCubeManager;
 import com.kleegroup.analytica.hcube.dimension.HCategoryPosition;
 import com.kleegroup.analytica.hcube.dimension.HTimeDimension;
 
@@ -13,13 +14,21 @@ import com.kleegroup.analytica.hcube.dimension.HTimeDimension;
  * @author npiedeloup, pchretien
  */
 public final class HQueryBuilder implements Builder<HQuery> {
+	private HCubeManager cubeManager;
 	private HTimeDimension timeDimension;
 	private Date from;
 	private Date to;
 	//----
 	private String type;
 	private String[] subCategories;
+	private int categoryLevel;
 
+	public HQueryBuilder(final HCubeManager cubeManager) {
+		Assertion.notNull(cubeManager);
+		//---------------------------------------------------------------------
+		this.cubeManager = cubeManager;		
+	}
+	
 	public HQueryBuilder on(HTimeDimension timeDimension) {
 		Assertion.notNull(timeDimension);
 		Assertion.isNull(this.timeDimension);
@@ -43,6 +52,12 @@ public final class HQueryBuilder implements Builder<HQuery> {
 		to = date;
 		return this;
 	}
+	
+	public HQueryBuilder categoryLevel(int categoryLevel) {
+		//---------------------------------------------------------------------
+		this.categoryLevel = categoryLevel;
+		return this;
+	}
 
 	public HQueryBuilder with(final String type, String... subCategories) {
 		Assertion.notNull(type);
@@ -53,8 +68,7 @@ public final class HQueryBuilder implements Builder<HQuery> {
 		return this;
 	}
 
-	@Override
 	public HQuery build() {
-		return new HQuery(new HTimeSelection(timeDimension, from, to), new HCategoryPosition(type, subCategories));
+		return new HQuery(new HTimeSelection(timeDimension,from,to), new HCategorySelection(cubeManager.getCategoryDictionary(), categoryLevel, new HCategoryPosition(type, subCategories)));
 	}
 }
