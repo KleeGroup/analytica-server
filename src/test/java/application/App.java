7245cdbcfcdcee46c7c6469c97cd8bc4eb09b89a
Philippe Chrétien <pchretien55@gmail.com>
2013-06-26 15:00:39 +0200

    Config en java

--- src/test/java/application/App.java
+++ src/test/java/application/App.java
@@ -2,8 +2,7 @@ package application;
 
 import java.util.Scanner;
 
-import kasper.kernel.Starter;
-import kasper.kernel.lang.Option;
+import kasper.kernel.Home;
 
 /**
  * 
@@ -14,19 +13,14 @@ import kasper.kernel.lang.Option;
  */
 public final class App {
 	public static void main(String[] args) {
-		//		final Injector injector = new Injector();
-		Starter starter = new Starter("./managers-test.xml", Option.<String> none(), App.class, 0L);
-		starter.start();
-
-		//On injecte les managers sur la classe de test.
-		//		injector.injectMembers(this, Home.getContainer());
+		Home.start(AppConfig.createHomeConfig());
 		try {
 			System.out.println("Taper sur entrée pour sortir");
 			final Scanner sc = new Scanner(System.in);
 			sc.nextLine();
 			sc.close();
 		} finally {
-			//Home.stop();
+			Home.stop();
 		}
 
 	}
