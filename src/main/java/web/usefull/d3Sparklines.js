8dbe2ca24b82c1d239afc180b7c8ffb25f9cb5b0
Philippe Chrétien <pchretien55@gmail.com>
2013-07-16 19:27:41 +0200

    exemple

---
+++ src/main/java/web/usefull/d3Sparklines.js
@@ -0,0 +1,27 @@
+<!--Script for bar sparklines-->
+var drawBarSparklines = function (data,id){
+	var space = 1; // space between the bars
+	var barWidth = 3;
+	var height = 10;
+	//----------------------------------------
+	var	width = (barWidth+space) * data.length;
+	var y = d3.scale.linear()
+		.domain([0, d3.max(data)])
+		.range([0, height]);
+
+	var rectangle = d3.selectAll(("."+id)).append("svg")
+		.attr("width", width)
+		.attr("height", height)
+		.attr("class","bar"); 
+
+	var rectangle = rectangle.selectAll("rect")
+		.data(data)
+		.enter().append("rect")
+			.attr("class", "bar")
+			.attr("width", barWidth)
+			.attr("height", y)
+			.attr("y", function(d) {return height-y(d) ;})
+			.attr("x", function(d, i) {return i * (barWidth+space);})
+			.style("fill", function(d, i) { return i==(data.length-1)? "#C00" : "#09C";})
+			.append("title").text(function(d) {return d;});
+}
