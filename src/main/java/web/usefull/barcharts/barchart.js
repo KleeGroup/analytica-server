09e3471da65ec728a2258ceb722865fe6e4744ff
Stephane TATCHUM <tksteph12@gmail.com>
2013-07-17 16:18:38 +0200

    d3sparkline update

--- src/main/java/web/usefull/barcharts/barchart.js
+++ src/main/java/web/usefull/barcharts/barchart.js
@@ -3,23 +3,22 @@ var drawBarChart = function(datas,id){
 	var defaultColors = ["#33B5E5", "#FF4444"];
 	var defaultHeight = 300;
 	var defaultWidth = 600;
-	var barWidth = 50;
-    var space = 10;
-    var axisSpacing = 3; // spacing between ticks
-   	//--------------------------
-    var margin = {
-      top: 20,
-      right: 50,
-      bottom: 50,
-      left: 50
-    }, width = undefined // width - margin.left - margin.right
-      ,
-      height = undefined // height -margin.top - margin.bottom
-      ,
-      color = null,
-      x, y1, y2;
+	var barWidth = 50;0
+	var space = 10;
+	var axisSpacing = 3; // spacing between ticks
+	//--------------------------
+	var margin = {
+		top: 20,
+		right: 50,
+		bottom: 50,
+		left: 50
+	}; 
+	var width = undefined; // width - margin.left - margin.right
+	var height = undefined, // height -margin.top - margin.bottom
+	var color = null;
+	var x, y1, y2;
 
-    var xAxis,
+	var xAxis,
       yLeftAxis,
       bars,
       legend;
@@ -92,79 +91,75 @@ var drawBarChart = function(datas,id){
 
     var parseToDate = function(d) {
       var format = d3.time.format.utc("%d/%m-%H:%M");
-
       return format(new Date(d[0]));
     };
 
     var parseToDate = function(d) {
       var format = d3.time.format.utc("%d/%m-%H:%M");
-
       return format(new Date(d[0]));
     };
+
     if (id === undefined) {
       id = container[0].id;
     }
 
-    var getTextY = function(d) {
-      if ((d === undefined) || ((d.key === undefined) && (d.unit === undefined))) {
-        throw "Labels and units not defined";
-      }
-      var text = "";
-      if (d.key === undefined) {
-        text += d.unit;
-      } else if (d.unit === undefined) {
-        text += d.key;
-      } else {
-        text = d.key + " ( " + d.unit + " )";
-      }
-
-      return text;
-
-    }
+	var getTextY = function(d) {
+		if ((d === undefined) || ((d.key === undefined) && (d.unit === undefined))) {
+			throw "Labels and units not defined";
+		}
+		var text = "";
+		if (d.key === undefined) {
+			text += d.unit;
+		} else if (d.unit === undefined) {
+			text += d.key;
+		} else {
+			text = d.key + " ( " + d.unit + " )";
+		}
+		return text;
+	};
 
 
-    var chart = d3.select('#' + id)
-      .append("svg")
-      .attr("width", width + margin.left + margin.right)
-      .attr("height", height + margin.top + margin.bottom)
-      .append("g")
-      .attr("transform", "translate(" + margin.left + "," + margin.top + ")"); // Draw X-axis grid lines
+	var chart = d3.select('#' + id).append("svg")
+		.attr("width", width + margin.left + margin.right)
+		.attr("height", height + margin.top + margin.bottom)
+		.append("g")
+			.attr("transform", "translate(" + margin.left + "," + margin.top + ")"); // Draw X-axis grid lines
 
-    chart.selectAll("line.x")
-      .data(x.ticks(10))
-      .enter().append("line")
-      .attr("class", "x")
-      .attr("x1", x)
-      .attr("x2", x)
-      .attr("y1", 0)
-      .attr("y2", height)
-      .style("stroke", "#ccc");
+	chart.selectAll("line.x")
+		.data(x.ticks(10))
+		.enter().append("line")
+			.attr("class", "x")
+			.attr("x1", x)
+			.attr("x2", x)
+			.attr("y1", 0)
+			.attr("y2", height)
+			.style("stroke", "#ccc");
 
-    // Draw Y-axis grid lines
-    chart.selectAll("line.y")
-      .data(yL.ticks(10))
-      .enter().append("line")
-      .attr("class", "y")
-      .attr("x1", 0)
-      .attr("x2", width)
-      .attr("y1", yL)
-      .attr("y2", yL)
-      .style("stroke", "#ccc");
+	// Draw Y-axis grid lines
+	chart.selectAll("line.y")
+	.data(yL.ticks(10))
+		.enter().append("line")
+			.attr("class", "y")
+			.attr("x1", 0)
+			.attr("x2", width)
+			.attr("y1", yL)
+			.attr("y2", yL)
+			.style("stroke", "#ccc");
 
     chart.append("g") // Add the X Axis
     .attr("class", "x axis")
       .attr("transform", "translate(0," + height + ")")
       .call(xAxis);
 
-    chart.append("g") // Add the Y Axis
-    .attr("class", "y axis")
-      .call(yLeftAxis)
-      .append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 2)
-      .attr("dy", ".71em")
-      .style("text-anchor", "end")
-      .text(getTextY(datas[0]));
+	chart.append("g") // Add the Y Axis
+		.attr("class", "y axis")
+		.call(yLeftAxis)
+		.append("text")
+			.attr("transform", "rotate(-90)")
+			.attr("y", 2)
+			.attr("dy", ".71em")
+			.style("text-anchor", "end")
+			.text(getTextY(datas[0]));
 
 
 
@@ -178,31 +173,21 @@ var drawBarChart = function(datas,id){
       return text;
     }
 
-    bars = chart.selectAll("rect")
-      .data(datas[0].values)
-      .enter().append("rect")
-      .attr("height", function(d) {
-      return height - yL(d[1]);
-    })
-      .attr("y", function(d) {
-      return yL(d[1])
-    })
-      .attr("x", function(d, i) {
-      return x((new Date(d[0]))) - barWidth / 2;
-    })
-      .attr("width", barWidth)
-      .style("fill", datas[0].color)
-      .style("stroke", "white")
-      .style("stroke-width", 1)
-      .on("mouseover", function(d) {
-      toolTip.transition().duration(200).style("opacity", 1)
-    })
-      .on("mousemove", function(d) {
-      toolTip.text(d[1] + " at " + parseToDate(d)).style("left", (d3.event.pageX - 34) + "px").style("top", (d3.event.pageY - 50) + "px");
-    })
-      .on("mouseout", function(d) {
-      toolTip.transition().duration(200).style("opacity", 1e-6);
-    });
+	bars = chart.selectAll("rect")
+		.data(datas[0].values)
+		.enter().append("rect")
+			.attr("height", function(d) {return height - yL(d[1]);})
+			.attr("y", function(d) { return yL(d[1])})
+			.attr("x", function(d, i) { return x((new Date(d[0]))) - barWidth / 2;})
+			.attr("width", barWidth)
+			.style("fill", datas[0].color)
+			.style("stroke", "white")
+			.style("stroke-width", 1)
+			.on("mouseover", function(d) { toolTip.transition().duration(200).style("opacity", 1)})
+			.on("mousemove", function(d) {
+				toolTip.text(d[1] + " at " + parseToDate(d)).style("left", (d3.event.pageX - 34) + "px").style("top", (d3.event.pageY - 50) + "px");
+			})
+			.on("mouseout", function(d) { toolTip.transition().duration(200).style("opacity", 1e-6);});
 
     //-----------Calcul de la taille de la légende
     
@@ -215,35 +200,28 @@ var drawBarChart = function(datas,id){
     var val = height + 3 * margin.bottom / 4;
     var legendHeight = margin.bottom / 2;
     // var legendWidth = width / 2;
-    legend.append("rect")
-      .attr("height", legendHeight)
-      .attr("width", legendWidth)
-      .attr("rx", 5)
-      .attr("ry", 5)
-      .attr("transform", "translate(0," + val + ")")
-      .style("fill", "#EEE");
+	
+	legend.append("rect")
+		.attr("height", legendHeight)
+		.attr("width", legendWidth)
+		.attr("rx", 5)
+		.attr("ry", 5)
+		.attr("transform", "translate(0," + val + ")")
+		.style("fill", "#EEE");
 
-    legend.selectAll('legend')
-      .data(datas)
-      .enter().append("rect")
-      .attr("y", height + margin.bottom / 2 + margin.top)
-      .attr("x", function(d, i) {
-      return i * 150;
-    })
-      .attr("width", legendHeight / 3)
-      .attr("height", legendHeight / 3)
-      .style("fill", function(d, i) {
-      return datas[datas.indexOf(d)].color;
-    });
+	legend.selectAll('legend')
+		.data(datas)
+		.enter().append("rect")
+			.attr("y", height + margin.bottom / 2 + margin.top)
+			.attr("x", function(d, i) { return i * 150;})
+			.attr("width", legendHeight / 3)
+			.attr("height", legendHeight / 3)
+			.style("fill", function(d, i) { return datas[datas.indexOf(d)].color;});
 
-    legend.selectAll('legend')
-      .data(datas)
-      .enter().append("text")
-      .attr("y", height + margin.bottom / 2 + margin.top + 7)
-      .attr("x", function(d, i) {
-      return i * 150 + 20;
-    })
-      .text(function(d) {
-      return datas[datas.indexOf(d)].key;
-    });
+	legend.selectAll('legend')
+		.data(datas)
+		.enter().append("text")
+			.attr("y", height + margin.bottom / 2 + margin.top + 7)
+			.attr("x", function(d, i) { return i * 150 + 20;})
+			.text(function(d) { return datas[datas.indexOf(d)].key;});
 }
