78cd9b502387e34e94b48f2e0603879bb3b93089
Stephane TATCHUM <tksteph12@gmail.com>
2013-07-17 15:53:35 +0200

    added barcharts

--- src/main/java/web/usefull/barcharts/barchart.js
+++ src/main/java/web/usefull/barcharts/barchart.js
@@ -1,4 +1,5 @@
-var drawLineBarChart = function(datas,id){
+var drawBarChart = function(datas,id){
+
 	var defaultColors = ["#33B5E5", "#FF4444"];
 	var defaultHeight = 300;
 	var defaultWidth = 600;
@@ -18,11 +19,8 @@ var drawLineBarChart = function(datas,id){
       color = null,
       x, y1, y2;
 
-    var container = document.getElementById(id);
     var xAxis,
-      yRightAxis,
       yLeftAxis,
-      line,
       bars,
       legend;
 
@@ -30,10 +28,8 @@ var drawLineBarChart = function(datas,id){
     if (datas[0].color === undefined) {
       datas[0].color = defaultColors[0];
     }
-    if (datas[1].color === undefined) {
-      datas[1].color = defaultColors[1];
-    }
-     // taille par défaut 
+
+    // taille par défaut 
     if ((height === undefined) || (height === 0)) {
       height = defaultHeight;
     }
@@ -41,9 +37,7 @@ var drawLineBarChart = function(datas,id){
       width = defaultWidth;
     }
 
-     
     var param = 0; // reduces the space of the chart according to that of it's container
-
     if (width < 300) {
       axisSpacing = 6;
       param = 7 / 10;
@@ -74,31 +68,23 @@ var drawLineBarChart = function(datas,id){
 
     x = d3.time.scale().range([0, width]);
     yL = d3.scale.linear().range([height, 0]);
-    yR = d3.scale.linear().range([height, 0]);
 
+    /*var minValue =  // find min and max value of the two ranges and use them for the x domain below
+                  ,maxValeu =*/
 
-    x.domain([new Date(d3.min(datas[1].values, function(d) {
+    x.domain([new Date(d3.min(datas[0].values, function(d) {
         return d[0];
-      }) - 3600000), d3.time.day.offset(new Date(d3.max(datas[1].values, function(d) {
+      }) - 3600000), d3.time.day.offset(new Date(d3.max(datas[0].values, function(d) {
         return d[0];
       }) + 3600000), 0)]);
 
     yL.domain([0, d3.max(datas[0].values, function(d) {
         return d[1];
       })])
-    yR.domain([0, d3.max(datas[1].values, function(d) {
-        return d[1];
-      })])
 
     xAxis = d3.svg.axis().scale(x).orient("bottom").tickFormat(d3.time.format.utc("%d/%m-%H:%M")).ticks(d3.time.hours, axisSpacing);
-    yRightAxis = d3.svg.axis().scale(yR).orient("right").ticks(5);
     yLeftAxis = d3.svg.axis().scale(yL).orient("left").ticks(5);
 
-    line = d3.svg.line().x(function(d) {
-      return x(d[0]);
-    }).y(function(d) {
-      return yR(d[1])
-    });
 
     var toolTip = d3.select("#" + id).append("div")
       .attr("class", "tooltip")
@@ -152,8 +138,7 @@ var drawLineBarChart = function(datas,id){
       .attr("x2", x)
       .attr("y1", 0)
       .attr("y2", height)
-      .style("stroke", "black")
-      .style("stroke-width", 0.1);
+      .style("stroke", "#ccc");
 
     // Draw Y-axis grid lines
     chart.selectAll("line.y")
@@ -164,8 +149,7 @@ var drawLineBarChart = function(datas,id){
       .attr("x2", width)
       .attr("y1", yL)
       .attr("y2", yL)
-      .style("stroke", "black")
-      .style("stroke-width", 0.1);
+      .style("stroke", "#ccc");
 
     chart.append("g") // Add the X Axis
     .attr("class", "x axis")
@@ -183,16 +167,6 @@ var drawLineBarChart = function(datas,id){
       .text(getTextY(datas[0]));
 
 
-    chart.append("g")
-      .attr("class", "y axis")
-      .attr("transform", "translate(" + width + " ,0)")
-      .call(yRightAxis)
-      .append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("y", -10)
-      .attr("dy", ".71em")
-      .style("text-anchor", "end")
-      .text(getTextY(datas[1]));
 
     var getToolTipText = function(d, datas) {
       var text = "";
@@ -224,56 +198,23 @@ var drawLineBarChart = function(datas,id){
       toolTip.transition().duration(200).style("opacity", 1)
     })
       .on("mousemove", function(d) {
-      toolTip.text(d[1] + " Le " + parseToDate(d)).style("left", (d3.event.pageX - 34) + "px").style("top", (d3.event.pageY - 50) + "px");
+      toolTip.text(d[1] + " at " + parseToDate(d)).style("left", (d3.event.pageX - 34) + "px").style("top", (d3.event.pageY - 50) + "px");
     })
       .on("mouseout", function(d) {
       toolTip.transition().duration(200).style("opacity", 1e-6);
     });
 
-    chart.append("svg:path")
-      .datum(datas[1].values)
-      .attr("d", line)
-      .attr("class", "line")
-      .style("stroke", datas[1].color);
-
-    var c = d3.scale.linear().domain([0, 1]).range([datas[1].color, datas[1].color]).interpolate(d3.interpolateHsl);
-    chart.selectAll("circle")
-      .data(datas[1].values)
-      .enter().append("svg:circle")
-      .attr("cx", function(d) {
-      return x(d[0])
-    })
-      .attr("cy", function(d) {
-      return yR(d[1])
-    })
-      .attr("stroke-width", "none")
-      .attr("fill", function() {
-      return c(Math.random())
-    })
-      .attr("fill-opacity", .5)
-      .attr("r", 3)
-      .style("display", "inline")
-      .on("mouseover", function(d) {
-      toolTip.transition().duration(200).style("opacity", 1);
-      d3.select(this).transition().duration(200).attr("fill-opacity", .5)
-    })
-      .on("mousemove", function(d) {
-      toolTip.text(getToolTipText(d, datas[0])).style("left", (d3.event.pageX - 34) + "px").style("top", (d3.event.pageY - 70) + "px");
-    })
-      .on("mouseout", function() {
-      toolTip.transition().duration(200).style("opacity", 1e-6);
-      d3.select(this).transition().duration(200)
-        .attr("fill-opacity", .5)
-    });
-
+    //-----------Calcul de la taille de la légende
+    
+    var legendWidth = width / 2;
 
     var legend = chart.append("g")
       .attr("class", "legend")
       .attr("transform", "translate(" + 0 + " ," + (-margin.top) + ")");
-   
+    
     var val = height + 3 * margin.bottom / 4;
     var legendHeight = margin.bottom / 2;
-    var legendWidth = width / 2;
+    // var legendWidth = width / 2;
     legend.append("rect")
       .attr("height", legendHeight)
       .attr("width", legendWidth)
