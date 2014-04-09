ef0f83c66c39861bde3b2f4ac7287de08a06ce30
pchretien@kleegroup.com <pchretien55@gmail.com>
2014-04-09 21:53:55 +0200

    perfs

--- analytica-server-hcube/src/main/java/io/analytica/hcube/cube/HMetricBuilder.java
+++ analytica-server-hcube/src/main/java/io/analytica/hcube/cube/HMetricBuilder.java
@@ -65,7 +65,7 @@ public final class HMetricBuilder implements Builder<HMetric> {
 		sum += value;
 		sqrSum += value * value;
 		if (metricKey.isClustered()) {
-			clusterValue(value);
+			incTreshold(clusterValue2(value), 1);
 		}
 		return this;
 	}
@@ -124,22 +124,45 @@ public final class HMetricBuilder implements Builder<HMetric> {
 		clusteredValues.put(treshold, incBy + (hcount == null ? 0 : hcount));
 	}
 
-	private void clusterValue(final double value) {
+	private double clusterValue2(final double value) {
 		//On crée une répartion : 1, 2, 5 - 10, 20, 50 - 100, 200, 500...
-		if (value <= 0) {
-			incTreshold(0, 1);
-		} else {
-			final double index = Math.floor(Math.log10(value));
-			final double treshold = Math.pow(10, index);
-			if (value <= treshold) {
-				incTreshold(treshold, 1);
-			} else if (value <= 2 * treshold) {
-				incTreshold(2 * treshold, 1);
-			} else if (value <= 5 * treshold) {
-				incTreshold(5 * treshold, 1);
-			} else {
-				incTreshold(10 * treshold, 1);
-			}
+		//Optim 
+		if (value <= 0)
+			return 0;
+		if (value <= 1)
+			return 1;
+		if (value <= 2)
+			return 2;
+		if (value <= 5)
+			return 5;
+		if (value <= 10)
+			return 10;
+		if (value <= 20)
+			return 20;
+		if (value <= 50)
+			return 50;
+		if (value <= 100)
+			return 100;
+		if (value <= 200)
+			return 200;
+		if (value <= 500)
+			return 500;
+		if (value <= 1000)
+			return 1000;
+		if (value <= 2000)
+			return 2000;
+		if (value <= 5000)
+			return 5000;
+		//Other cases
+		final double index = Math.floor(Math.log10(value));
+		final double treshold = Math.pow(10, index);
+		if (value <= treshold) {
+			return treshold;
+		} else if (value <= 2 * treshold) {
+			return 2 * treshold;
+		} else if (value <= 5 * treshold) {
+			return 5 * treshold;
 		}
+		return 10 * treshold;
 	}
 }
